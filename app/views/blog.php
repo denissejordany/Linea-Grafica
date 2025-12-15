902d1398e2fc51feefacfe98d9b88a597108c0f7
denissejordany <denissejordany@gmail.com>
2025-12-15 12:09:15 -0500

    Se agregaron todos los pdf de las politicas que iran en el bloque Nuestras Politicas

--- app/views/blog.php
+++ app/views/blog.php
@@ -1,5 +1,40 @@
-
-
-<h1>Blog</h1>
-
-
+<!----------SOBRE NOSOTROS-------->
+<h2 class="info-title">
+        <span>SOBRE NOSOTROS</span>
+   </h2>
+   <!----------FOTOS DE LAS INSTALACIONES-------->
+<h2 class="info-title">
+        <span>FOTOS DE LAS INSTALACIONES</span>
+   </h2>
+   <!----------PROYECTOS-------->
+<h2 class="info-title">
+        <span>PROYECTOS</span>
+   </h2>
+     <div class="cotizar-wrapper">
+    <a href="#cotizar" class="btn-cotizar">Cotizar ahora</a>
+</div>
+      <!---------- PREGUNTAS FRECUENTES-------->
+<h2 class="info-title">
+        <span> PREGUNTAS FRECUENTES</span>
+   </h2>
+      <!----------¿QUÉ ES UN BROCHURE?-------->
+<h2 class="info-title">
+        <span>¿QUÉ ES UN BROCHURE?</span>
+   </h2>
+     <div class="cotizar-wrapper">
+    <a href="#cotizar" class="btn-cotizar">Cotizar ahora</a>
+</div>
+      <!----------CONOCE A LÍNEA GRÁFICA-------->
+<h2 class="info-title">
+        <span>CONOCE A LÍNEA GRÁFICA</span>
+   </h2>
+     <div class="cotizar-wrapper">
+    <a href="#cotizar" class="btn-cotizar">Cotizar ahora</a>
+</div>
+      <!----------¿LOS TRABAJOS SON MEJOR EN RGB o CMYK?-------->
+<h2 class="info-title">
+        <span>¿LOS TRABAJOS SON MEJOR EN RGB o CMYK?</span>
+   </h2>
+     <div class="cotizar-wrapper">
+    <a href="#cotizar" class="btn-cotizar">Cotizar ahora</a>
+</div>
