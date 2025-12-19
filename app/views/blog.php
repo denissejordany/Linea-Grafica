a5e83dde7c7fe827b431060ddbf20e929495320a
denissejordany <denissejordany@gmail.com>
2025-12-18 23:53:02 -0500

    carruseles de blog y avance de preguntas frecuentes

--- app/views/blog.php
+++ app/views/blog.php
@@ -44,7 +44,7 @@
 <section class="alt-carousel">
     <div class="alt-carousel-inner">
 
-        <button class="alt-btn alt-prev">&#10094;</button>
+       
 
         <div class="alt-track">
             <!-- Slide 1 -->
@@ -91,8 +91,7 @@
              <!-- Slide 5 -->
             <div class="alt-slide">
                 <div class="alt-text">
-                    DEDICACIÓN <br>
-EN CADA PIEZA
+                    DEDICACIÓN <br>EN CADA PIEZA
                 </div>
                 <div class="alt-image">
                     <img src="<?php echo Routes::img('blog/foto5.png'); ?>">
@@ -110,16 +109,16 @@ EN CADA PIEZA
             </div>
         </div>
 
-       <button type="button" class="alt-btn alt-prev" aria-label="Slide anterior">
-    &#10094;
-</button>
+            <button type="button" class="alt-btn alt-prev" aria-label="Slide anterior">
+          &#10094;
+          </button>
+ 
+              <button type="button" class="alt-btn alt-next" aria-label="Slide siguiente">
+             &#10095;
+                  </button>
 
-<button type="button" class="alt-btn alt-next" aria-label="Slide siguiente">
-    &#10095;
-</button>
 
-
-    </div>
+   </div>
 </section>
 
 
@@ -127,6 +126,221 @@ EN CADA PIEZA
 <h2 class="info-title">
         <span>PROYECTOS</span>
    </h2>
+   <section class="projects-carousel">
+    <div class="projects-carousel-inner">
+
+        <button type="button" class="projects-btn projects-prev" aria-label="Proyecto anterior">
+            &#10094;
+        </button>
+
+        <div class="projects-track">
+
+            <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto1.png'); ?>" alt="Proyecto 1">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Cuadernos y brochure</h3>
+                    <p class="projects-text">
+                        Le garantizamos la máxima calidad en cada tirada junto a un 
+                        servicio inigualable para todos sus proyectos de cuadernos y brochures.
+                    </p>
+                </div>
+            </div>
+
+            <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto2.png'); ?>" alt="Proyecto 2">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Individuales personalisados</h3>
+                    <p class="projects-text">
+                        Ofrecemos individuales que no solo añaden un toque de distinción a sus platos y productos,
+                         sino que también cumplen con los más altos estándares de calidad gráfica.
+                    </p>
+                </div>
+            </div>
+  <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto3.png'); ?>" alt="Proyecto 3">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Collarines y cadenetas</h3>
+                    <p class="projects-text">
+                        Le proporcionamos las soluciones más actuales y eficientes en cadenetas
+                         y collarines para destacar sus productos en el mercado
+                    </p>
+                </div>
+            </div>
+           
+             <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto5.png'); ?>" alt="Proyecto 5">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Banners</h3>
+                    <p class="projects-text">
+                        Mientras la competencia invierte en visibilidad, nosotros le ofrecemos 
+                        la garantía de un producto de línea gráfica inigualable.
+                    </p>
+                </div>
+            </div>
+             <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto6.png'); ?>" alt="Proyecto 6">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Raspa y ganal</h3>
+                    <p class="projects-text">
+                        Le ofrecemos una solución completa para sus boletos "Raspa y Gana",
+                         asegurando calidad de impresión y manejo de la aleatoriedad de su proyecto
+                    </p>
+                </div>
+            </div>
+             <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto7.png'); ?>" alt="Proyecto 7">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Volantes y enmicados</h3>
+                    <p class="projects-text">
+                        Asegure que sus proyectos cuenten con la solidez de volantes impresos bajo los mejores estándares de calidad.
+                        El enmicado ofrece una inversión inteligente al preservar la integridad de sus documentos importantes.
+                    </p>
+                </div>
+            </div>
+             <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto8.png'); ?>" alt="Proyecto 8">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Marcador de precios</h3>
+                    <p class="projects-text">
+                        Un marcador de precios es un elemento vital para la transparencia y eficiencia operativa.
+                         Es crucial disponer de marcadores de calidad que faciliten la señalización de sus productos.
+                    </p>
+                </div>
+            </div>
+             <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto9.png'); ?>" alt="Proyecto 9">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Paleta - tablet tent</h3>
+                    <p class="projects-text">
+                       Nuestras paletas Tablet Tent son la solución ideal para comunicar ofertas y mensajes clave 
+                       directamente en el punto de decisión del cliente.
+                    </p>
+                </div>
+            </div>
+             <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto10.png'); ?>" alt="Proyecto 10">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Cajas - display</h3>
+                    <p class="projects-text">
+                        Ofrecemos soluciones de empaque que se convierten en plataformas de marketing. Garantizamos la 
+                        calidad de la línea gráfica y la integridad estructural de sus cajas display.
+                    </p>
+                </div>
+            </div>
+             <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto11.png'); ?>" alt="Proyecto 11">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Rompecabezas</h3>
+                    <p class="projects-text">
+                        Una manera creativa y tangible de comunicar su marca. Contamos con la mejor calidad para que su 
+                        diseño encaje perfectamente. Cotice su rompecabezas personalizado hoy.
+                    </p>
+                </div>
+            </div>
+             <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto12.png'); ?>" alt="Proyecto 12">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Stickers</h3>
+                    <p class="projects-text">
+                        Los stickers son una herramienta de marketing simple pero poderosa para incrementar la visibilidad 
+                        de su marca. Ofrecemos alta calidad en impresión y adhesivos duraderos.
+                    </p>
+                </div>
+            </div>
+             <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto13.png'); ?>" alt="Proyecto 13">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Cupones</h3>
+                    <p class="projects-text">
+                       Asegure que su estrategia de descuentos se ejecute con materiales de alta calidad y acabado profesional. 
+                    </p>
+                </div>
+            </div>
+               <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto14.png'); ?>" alt="Proyecto 14">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Afiches</h3>
+                    <p class="projects-text">
+                        Ponga su evento, promoción o anuncio en el foco con afiches producidos bajo los más altos
+                         estándares de nuestra línea gráfica. 
+                    </p>
+                </div>
+            </div>
+  <!-- Slide -->
+            <div class="projects-slide">
+                <div class="projects-image">
+                    <img src="<?php echo Routes::img('blog/proyectos/proyecto4.png'); ?>" alt="Proyecto 4">
+                </div>
+
+                <div class="projects-content">
+                    <h3 class="projects-subtitle">Cartas menú</h3>
+                    <p class="projects-text">
+                        Asegure que cada detalle de su oferta se comunique con elegancia y claridad con colores
+                         vibrantes, textos legibles y materiales de primer nivel que reflejan la excelencia de su cocina.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <button type="button" class="projects-btn projects-next" aria-label="Proyecto siguiente">
+            &#10095;
+        </button>
+
+    </div>
+</section>
+
      <div class="cotizar-wrapper">
     <a href="#cotizar" class="btn-cotizar">Cotizar ahora</a>
 </div>
@@ -134,6 +348,40 @@ EN CADA PIEZA
 <h2 class="info-title">
         <span> PREGUNTAS FRECUENTES</span>
    </h2>
+   <section class="faq-highlight">
+    <div class="faq-highlight-container">
+
+        <!-- Imagen izquierda -->
+        <div class="faq-highlight-image">
+            <img src="<?php echo Routes::img('blog/pregunta1.png'); ?>" alt="Brochure ejemplo">
+        </div>
+
+        <!-- Contenido derecho -->
+        <div class="faq-highlight-content">
+            <h3 class="faq-highlight-title">¿Qué es un brochure?</h3>
+
+            <p class="faq-highlight-text">
+                Un brochure es un documento impreso o digital que tiene como objetivo
+                divulgar y describir una entidad, organización, producto, servicio
+                o evento en particular.
+            </p>
+
+           <button class="faq-highlight-btn-text" type="button">
+    Conoce más
+    <img
+        src="<?php echo Routes::img('blog/bt_conoce.png'); ?>"
+        alt=""
+        class="faq-highlight-icon"
+    >
+</button>
+
+
+        </div>
+
+    </div>
+</section>
+
+
       <!----------¿QUÉ ES UN BROCHURE?-------->
 <h2 class="info-title">
         <span>¿QUÉ ES UN BROCHURE?</span>
